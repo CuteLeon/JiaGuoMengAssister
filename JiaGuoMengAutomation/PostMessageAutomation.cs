28a18754f485527e07ae3b394677095544f3c7d6
Leon <leon.id@qq.com>
2019-11-06 19:33:50 +0800

    使用同步API SendMessage 代替异步API PostMessage；修正建筑坐标为地基中心位置；

--- JiaGuoMengAutomation/PostMessageAutomation.cs
+++ JiaGuoMengAutomation/PostMessageAutomation.cs
@@ -7,14 +7,13 @@ namespace JiaGuoMengAutomation
 {
     public class PostMessageAutomation : AutomationBase
     {
-        // TODO: 测试 SendMessage 同步效果是否可以去掉 Sleep()
-        [DllImport("user32", EntryPoint = "PostMessage", SetLastError = false, CharSet = CharSet.Auto, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
-        public static extern int PostMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        [DllImport("user32", EntryPoint = "SendMessage", SetLastError = false, CharSet = CharSet.Auto, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
 
         [DllImport("user32", EntryPoint = "FindWindow", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
         public static extern int FindWindow(string lpClassName, string lpWindowName);
 
-        [DllImport("user32", EntryPoint = "FindWindowEx", SetLastError = false, CharSet = CharSet.Auto, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        [DllImport("user32", EntryPoint = "FindWindowEx", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
         public static extern int FindWindowEx(int hWnd1, int hWnd2, string lpsz1, string lpsz2);
 
         public PostMessageAutomation()
@@ -25,17 +24,17 @@ namespace JiaGuoMengAutomation
             this.Locations = new LocationCollection();
             this.Locations.BuildingsLocations = new List<Point>()
             {
-                new Point(108, 273),
-                new Point(108, 363),
-                new Point(108, 455),
+                new Point(108, 262),
+                new Point(108, 352),
+                new Point(108, 440),
 
-                new Point(200, 227),
-                new Point(200, 319),
-                new Point(200, 408),
+                new Point(200, 216),
+                new Point(200, 305),
+                new Point(200, 395),
 
-                new Point(290, 182),
-                new Point(290, 272),
-                new Point(290, 364),
+                new Point(290, 170),
+                new Point(290, 260),
+                new Point(290, 350),
             };
             this.Locations.GiftsLocations = new List<Point>()
             {
@@ -43,6 +42,7 @@ namespace JiaGuoMengAutomation
                 new Point(295, 565),
                 new Point(350, 535),
             };
+            this.Locations.EmptyLocation = new Point(190, 75);
         }
 
         private IntPtr GetRenderWindowHandle()
@@ -80,7 +80,7 @@ namespace JiaGuoMengAutomation
 
         public void MouseLeftDown(int lparam)
         {
-            PostMessage(this.TargetHandle, 0x201, 1, lparam);
+            SendMessage(this.TargetHandle, 0x201, 1, lparam);
         }
 
         public override void MouseLeftUp(int x, int y)
@@ -91,13 +91,13 @@ namespace JiaGuoMengAutomation
 
         public void MouseLeftUp(int lparam)
         {
-            PostMessage(this.TargetHandle, 0x202, 0, lparam);
+            SendMessage(this.TargetHandle, 0x202, 0, lparam);
         }
 
         public override void MouseMove(int x, int y)
         {
             int lparam = this.GetLParam(x, y);
-            PostMessage(this.TargetHandle, 0x200, 0, lparam);
+            SendMessage(this.TargetHandle, 0x200, 0, lparam);
         }
     }
 }
