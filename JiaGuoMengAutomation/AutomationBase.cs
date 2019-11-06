0201b487aab79e1fee54d124fb787f5ee1ca022b
Leon <leon.id@qq.com>
2019-11-06 18:33:11 +0800

    增加自动化基类；

---
+++ JiaGuoMengAutomation/AutomationBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+
+namespace JiaGuoMengAutomation
+{
+    public abstract class AutomationBase : IAutomation
+    {
+        public IntPtr TargetHandle { get; set; }
+
+        public LocationCollection Locations { get; set; }
+
+        public CancellationToken Token { get; set; }
+
+        public virtual void ClickBuildings()
+        {
+            this.Locations.BuildingsLocations.ForEach((building) =>
+            {
+                this.MouseClick(building.X, building.Y);
+                Thread.Sleep(20);
+            });
+        }
+
+        public virtual void ClickEmpty()
+        {
+            this.MouseClick(this.Locations.EmptyLocation.X, this.Locations.EmptyLocation.Y);
+        }
+
+        public virtual void DragGifts()
+        {
+            const int time = 100;
+            foreach (Point gift in this.Locations.GiftsLocations)
+            {
+                foreach (Point building in this.Locations.BuildingsLocations)
+                {
+                    // 及时退出
+                    if (this.Token.IsCancellationRequested)
+                    {
+                        return;
+                    }
+
+                    for (int index = 0; index < 3; index++)
+                    {
+                        this.MouseMove(gift.X, gift.Y);
+                        Thread.Sleep(time);
+                        this.MouseLeftDown(-1, -1);
+                        Thread.Sleep(time);
+                        this.MouseMove(building.X, building.Y);
+                        Thread.Sleep(time);
+                        this.MouseLeftUp(-1, -1);
+                        Thread.Sleep(time);
+                    }
+                }
+            }
+        }
+
+        public abstract void MouseClick(int x, int y);
+
+        public abstract void MouseLeftDown(int x, int y);
+
+        public abstract void MouseLeftUp(int x, int y);
+
+        public abstract void MouseMove(int x, int y);
+    }
+}
--- JiaGuoMengAutomation/PostMessageAutomation.cs
+++ JiaGuoMengAutomation/PostMessageAutomation.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Threading;
 
 namespace JiaGuoMengAutomation
 {
-    public class PostMessageAutomation : IAutomation
+    public class PostMessageAutomation : AutomationBase
     {
         // TODO: 测试 SendMessage 同步效果是否可以去掉 Sleep()
         [DllImport("user32", EntryPoint = "PostMessage", SetLastError = false, CharSet = CharSet.Auto, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
         public static extern int PostMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
-
-        public IntPtr TargetHandle { get; set; }
-
-        public LocationCollection Locations { get; set; }
-
-        public CancellationToken Token { get; set; }
 
         public PostMessageAutomation()
         {
@@ -31,59 +23,19 @@ namespace JiaGuoMengAutomation
             return IntPtr.Zero;
         }
 
-        public void ClickBuildings()
-        {
-            this.Locations.BuildingsLocations.ForEach((building) =>
-            {
-                this.MouseClick(building.X, building.Y);
-                Thread.Sleep(20);
-            });
-        }
-
-        public void ClickEmpty()
-        {
-            this.MouseClick(this.Locations.EmptyLocation.X, this.Locations.EmptyLocation.Y);
-        }
-
-        public void DragGifts()
-        {
-            const int time = 100;
-            foreach (Point gift in this.Locations.GiftsLocations)
-            {
-                foreach (Point building in this.Locations.BuildingsLocations)
-                {
-                    // 及时退出
-                    if (this.Token.IsCancellationRequested)
-                    {
-                        return;
-                    }
-
-                    for (int index = 0; index < 3; index++)
-                    {
-                        this.MouseMove(gift.X, gift.Y);
-                        Thread.Sleep(time);
-                        this.MouseLeftDown(-1, -1);
-                        Thread.Sleep(time);
-                        this.MouseMove(building.X, building.Y);
-                        Thread.Sleep(time);
-                        this.MouseLeftUp(-1, -1);
-                        Thread.Sleep(time);
-                    }
-                }
-            }
-        }
-
         public int GetLParam(int x, int y)
-            => y << 16 | x;
+        {
+            return y << 16 | x;
+        }
 
-        public void MouseClick(int x, int y)
+        public override void MouseClick(int x, int y)
         {
             int lparam = this.GetLParam(x, y);
             this.MouseLeftDown(lparam);
             this.MouseLeftUp(lparam);
         }
 
-        public void MouseLeftDown(int x, int y)
+        public override void MouseLeftDown(int x, int y)
         {
             int lparam = this.GetLParam(x, y);
             this.MouseLeftDown(lparam);
@@ -94,7 +46,7 @@ namespace JiaGuoMengAutomation
             PostMessage(this.TargetHandle, 0x201, 1, lparam);
         }
 
-        public void MouseLeftUp(int x, int y)
+        public override void MouseLeftUp(int x, int y)
         {
             int lparam = this.GetLParam(x, y);
             this.MouseLeftUp(lparam);
@@ -105,7 +57,7 @@ namespace JiaGuoMengAutomation
             PostMessage(this.TargetHandle, 0x202, 0, lparam);
         }
 
-        public void MouseMove(int x, int y)
+        public override void MouseMove(int x, int y)
         {
             int lparam = this.GetLParam(x, y);
             PostMessage(this.TargetHandle, 0x200, 0, lparam);
