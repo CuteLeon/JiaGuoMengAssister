c8d7c661dae236ce3941aa500121402289ea36f8
Leon <leon.id@qq.com>
2019-11-07 16:21:38 +0800

    增加蓝叠模拟器自动化支持类；

---
+++ JiaGuoMengAutomation/BlueStacksAutomation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace JiaGuoMengAutomation
+{
+    public class BlueStacksAutomation : AutomationBase
+    {
+        [DllImport("user32", EntryPoint = "SendMessage", SetLastError = false, CharSet = CharSet.Auto, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("user32", EntryPoint = "FindWindowEx", SetLastError = false, CharSet = CharSet.Unicode, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+        public static extern int FindWindowEx(int hWnd1, int hWnd2, string lpsz1, string lpsz2);
+
+        public BlueStacksAutomation()
+        {
+            this.TargetHandle = this.GetWindowHandle();
+
+            // TODO: 读取分辨率，以 360x640 为准
+            this.Locations = new LocationCollection();
+            this.Locations.BuildingsLocations = new List<Point>()
+            {
+            };
+            this.Locations.GiftsLocations = new List<Point>()
+            {
+            };
+            this.Locations.EmptyLocation = new Point();
+        }
+
+        private IntPtr GetWindowHandle()
+        {
+            int handle = FindWindowEx(0, 0, "BS2CHINAUI", "BlueStacks App Player");
+            return new IntPtr(handle);
+        }
+
+
+        public override void MouseClick(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MouseLeftDown(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MouseLeftUp(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MouseMove(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
